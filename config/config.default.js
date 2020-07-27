95ff03c28462e600e28da09eb212a74e97019564
zhangguanghui <zhangguanghui@smartcinema.com.cn>
2020-07-27 15:33:06 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  /**
+   * some description
+   * @member Config#test
+   * @property {String} key - some description
+   */
+  config.test = {
+    key: appInfo.name + '_123456',
+  };
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.html': 'nunjucks',
+    },
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 8001,
+      hostname: '0.0.0.0',
+    },
+  };
+
+  config.devServer = appInfo.env === 'local' && {
+    port: 8002,
+  };
+
+  config.httpProxy = appInfo.env === 'local' && {
+    '/assets': `http://localhost:${config.devServer.port}`,
+    '/sockjs-node': `http://localhost:${config.devServer.port}`,
+  };
+
+  config.development = {
+    reloadPattern: [ '**', '!**/web/**/*.*' ],
+  };
+
+  config.static = appInfo.env === 'prod' ? {
+    prefix: '/assets/',
+    dir: path.join(appInfo.baseDir, 'app/web/dist'),
+  } : {};
+
+  return config;
+};
---
+++ config/plugin.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// add you build-in plugin here, example:
+// exports.nunjucks = {
+//   enable: true,
+//   package: 'egg-view-nunjucks',
+// };
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
+};
+
+exports.httpProxy = {
+  enable: true,
+  package: 'egg-http-proxy',
+};
+
